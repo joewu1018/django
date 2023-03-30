589d17e4cdda5304ec63c826b6d851ecdb08666a
joewu1018 <s110913212@stu.ntue.edu.tw>
2023-03-30 23:42:14 +0800

    新增githun host

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9@(^9p%f7#hqu+yu+9a*a%u9tyno3q3*xsewbjqxwuqs^ite72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [joewu1018.github.io]
 
 
 # Application definition
